0c857a305938d9065f58b5650c4183e4c89e4217
Nacho Cabanes <nacho@nachocabanes.com>
2018-06-04 12:29:07 +0200

    Hello World: Java style braces

--- basic/HelloWorld.java
+++ basic/HelloWorld.java
@@ -2,10 +2,8 @@
 // Part of JavaSnippets
 // https://github.com/ncabanes/javaSnippets
 
-public class HelloWorld
-{
-    public static void main(String[] args)
-    {
+public class HelloWorld {
+    public static void main(String[] args) {
         System.out.println("Hello, world!");
     }
 }
