c7cb868c4696f9b000a1ec1b4b80bdf07474f400
Nacho Cabanes <nacho@nachocabanes.com>
2018-06-12 09:43:03 +0200

    ForContinue : skipping an iteration of a "for" loop (updated)

--- basic/ForContinue.java
+++ basic/ForContinue.java
@@ -2,14 +2,21 @@
 // Part of JavaSnippets
 // https://github.com/ncabanes/javaSnippets
 
-class ForContinue { 
-
+class ForContinue {
     public static void main( String args[] ) { 
+        
+        // Output: 1 2 3 4 5 7 8 9 10
+        
         for ( int i = 1 ; i <= 10 ; i++ ) {
              if (i == 6) 
                  continue;
              System.out.print( i + " " );
         }
+        
+        // An alternative way, avoiding "continue":
+        for ( int i = 1 ; i <= 10 ; i++ ) {
+             if (i != 6) 
+                System.out.print( i + " " );
+        }
     }
-    // Output: 1 2 3 4 5 7 8 9 10
 }
