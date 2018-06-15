746f5a6c047bbfab098ebdc2e74d92ae46f3822a
Nacho Cabanes <nacho@nachocabanes.com>
2018-06-15 23:47:16 +0200

    Example of the ternary operator

---
+++ basic/Ternary.java
@@ -0,0 +1,14 @@
+// Example of the ternary operator
+// Part of JavaSnippets
+// https://github.com/ncabanes/javaSnippets
+
+public class Ternary {
+    public static void main(String[] args) {
+        
+        int n = 15;
+        
+        System.out.print("Java has ");
+        System.out.print( n > 10 ? "many" : "a few");
+        System.out.println(" interesting details");
+    }
+}
